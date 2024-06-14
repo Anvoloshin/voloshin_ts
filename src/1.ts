aef5ab06cc4b38e13bd1406f78b65222ac82b5f8
a.voloshin <a.voloshin@tg.dunice.net>
2024-06-14 12:23:20 +0300

    first task

--- src/1.ts
+++ src/1.ts
@@ -1,18 +1,17 @@
-const flattenConstructor = <T>() => {
-  return (dict: Option<T>)=> Object.values(dict);
-  // для возвращаемого
-  // generic
-  // типизация словарем
+const flattenConstructor = <T>() : ReturnFunctionType<T>  => {
+  return (dict: Option<T>) : T[] => Object.values(dict);
 };
 
-interface Option<T> {
- [key: string]: T;
-}
+type ReturnFunctionType<T> = (dict: Option<T>) => T[];
 
-const flat1 = flattenConstructor();
+interface Option<T> {
+  [key: string]: T;
+ }
+
+const flat1 = flattenConstructor<number>();
 const r1 = flat1({ a: 1, b: 2 });
 console.log(r1);
 
-const flat2 = flattenConstructor();
+const flat2 = flattenConstructor<string>();
 const r2 = flat2({ a: '1', b: '2' });
 console.log(r2);
